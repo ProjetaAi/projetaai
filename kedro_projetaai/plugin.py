592851c7a46e867876475f4f4d349982f765b19c
nickolasrm <nickolas.machado@ipiranga.ipiranga>
2022-07-29 22:45:48 +0000

    moved overrides

--- kedro_projetaai/plugin.py
+++ kedro_projetaai/plugin.py
@@ -1,16 +1,8 @@
 """Kedro plugin interface."""
 from kedro_projetaai.cli.cli import setup_cli
 from kedro_projetaai.overrides import (
-    override_get_credentials
+    ProjetaAiOverrides,
 )
-
-
-class ProjetaAiOverrides:
-    """Fake Kedro hook to enable ProjetaAi overrides."""
-
-    def __init__(self):
-        """Initialize ProjetaAi overrides."""
-        override_get_credentials()
 
 
 overrides = ProjetaAiOverrides()
