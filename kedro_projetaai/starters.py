e95d467cf50a58baae53a7f982f5974beff96414
Gustavo Machado <gustavo.machado@advisia.com>
2022-12-27 18:28:51 -0300

    commit to pull

--- kedro_projetaai/starters.py
+++ kedro_projetaai/starters.py
@@ -3,10 +3,22 @@ from kedro.framework.cli.starters import KedroStarterSpec
 
 STARTERS_REPO = 'git+https://github.com/ProjetaAi/projetaai-starters.git'
 
+# project_starters = [
+#     KedroStarterSpec(
+#         alias='projetaai',
+#         template_path=STARTERS_REPO,
+#         directory='for_projetaai/project/projetaai',
+#     ),
+#     KedroStarterSpec(
+#         alias='projetaai-multipipeline',
+#         template_path=STARTERS_REPO,
+#         directory='multipipeline',
+#     ),
+# ]
+
 project_starters = [
     KedroStarterSpec(
-        alias='projetaai',
-        template_path=STARTERS_REPO,
-        directory='for_projetaai/project/projetaai',
+        alias='projetaai-multipipeline',
+        template_path="C:\\Users\\gustavo.machado\\Documents\\06_dev\\projetaai-starters\\multipipeline"
     )
 ]
